936cd446b3e821a7ba67b1918b482841fea4dc07
branmalawi <branmalawi@gmail.com>
2023-03-09 17:51:29 +0700

    set validator for auth/register api

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,12 @@
+const { validationResult } = require("express-validator");
+
 exports.register = (req, res) => {
   const { username, email } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   res.json({
     message: "account registered successfully",
     data: {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 const authController = require("../controller/authController");
 
-router.post("/register", authController.register);
+const optionPassword = {
+  minLength: 8,
+};
+
+router.post(
+  "/register",
+  body("email").isEmail(),
+  body("email").isLength({ min: 15 }),
+  body("password").isLength({ min: 5 }),
+  authController.register
+);
 
 module.exports = router;
